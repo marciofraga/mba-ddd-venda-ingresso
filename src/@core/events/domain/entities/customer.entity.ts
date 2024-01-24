179dcf6016a345d7713ac912b3ac18e4fa317896
Marcio Fraga <marcior.mfraga@gmail.com>
2024-01-24 14:42:51 -0300

    creating first entity

---
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -0,0 +1,33 @@
+import { Entity } from '../../../common/domain/Entity';
+import { AggregateRoot } from '../../../common/domain/aggregate-root';
+
+export type CustomerConstructorProps = {
+  id?: string;
+  cpf: string;
+  name: string;
+}
+
+export class Customer extends AggregateRoot{
+  id: string;
+  cpf: string;
+  name: string;
+
+  constructor(props: CustomerConstructorProps) {
+    super();
+    this.id = props.id;
+    this.cpf = props.cpf;
+    this.name = props.name;
+  }
+
+  static create(command: {name: string; cpf: string}): Customer {
+    return new Customer(command);
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      cpf: this.cpf,
+      name: this.name
+    };
+  }
+}
